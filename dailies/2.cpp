d314a2ab4e8f6ae37374f140df342e480369570d
Tere Angulo <twilishiba@gmail.com>
2024-01-02 12:23:40 -0600

    dailies 2/365

---
+++ dailies/2.cpp
@@ -0,0 +1,21 @@
+// 2610. Convert an Array Into a 2D Array With Conditions
+// angulo.teresa
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        vector<int> count(nums.size());
+        vector<vector<int>> ans;
+        for (int i = 0; i < nums.size(); ++i){
+            int n = nums[i];
+            if (count[n - 1] == ans.size())
+                ans.push_back({n});
+            else
+                ans[count[n - 1]].push_back(n);
+            count[n - 1]++;
+        }
+        return ans;
+    }
+};
